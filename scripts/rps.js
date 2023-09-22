54069c52944605b6a3b84949d570e37fe70913d7
Hoàng Minh Đức <hoangminhduc0308@gmail.com>
2023-09-22 11:31:42 +0700

    fixed game logic error

--- scripts/rps.js
+++ scripts/rps.js
@@ -5,23 +5,28 @@ function get_computer_choice() {
 
 player_input = "";
 
+for (i of ["rock", "paper", "scissors"]){
+    for (j of ["rock", "paper", "scissors"]){
+        if (i == j) console.log("")
+    }
+}
 
 // player_input = "rock";
 function game(pl) {
     var computer_input = get_computer_choice();
     var winner = "";
     if (pl != computer_input) {
-        switch (pl) {
-            case "rock":
-                winner = (computer_input == "paper") ? "Computer" : "You";
-                break;
-            case "paper":
-                winner = (computer_input == "scissors") ? "Computer" : "You";
-                break;
-            case "scissors":
-                winner = (computer_input == "rock") ? "Computer" : "You";
-                break;
-        }
+        if (pl == "rock" && computer_input == "paper") winner = "Computer wins!";
+        if (pl == "rock" && computer_input == "scissors") winner = "You win!";
+        if (pl == "rock" && computer_input == "rock") winner = "Tie";
+
+        if (pl == "paper" && computer_input == "paper") winner = "Tie!";
+        if (pl == "paper" && computer_input == "scissors") winner = "Computer wins!";
+        if (pl == "paper" && computer_input == "rock") winner = "You win!";
+
+        if (pl == "scissors" && computer_input == "paper") winner = "You win!";
+        if (pl == "scissors" && computer_input == "scissors") winner = "Tie!";
+        if (pl == "scissors" && computer_input == "rock") winner = "Computer wins!";
     }
     if (pl === computer_input) {
         winner = "Tie!";
@@ -30,6 +35,7 @@ function game(pl) {
     } else {
         winner = "Computer wins!"
     }
+    
     document.getElementById("result").innerHTML = "";
     document.getElementById("result").innerHTML += "<h2>You chose: " + pl + "</h2>";
     document.getElementById("result").innerHTML += "<h2>Computer chose: " + computer_input + "</h2>";
